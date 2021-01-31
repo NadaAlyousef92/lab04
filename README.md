ca48d57d8f4c53b7fdcd57101f3a44ceec2e7170
NadaAlyousef92 <nada.alyousef92@gmail.com>
2021-01-31 20:20:04 +0200

    test3

--- README.md
+++ README.md
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<head>
-    <body>
 
-<h1>style="color:red;" Welcome to Queen events co.</h1>
-
-
-
-
-
-
-
-
-    </body>
-    
-</head>
-<footer>Designed by Nada Alyousef </footer>
--- java.js
+++ java.js
@@ -1,43 +1,47 @@
 
 
-        <var orderlist 
-        
+var order;
+
+while (order !== "wedding" && order == "reception") {
+    order = prompt("what is your celebration wedding or reception?");
+}
+
+
+
+
+var order = prompt('what do you have wedding or reception?');
+
+while (order !== " reception " && order == " wedding") {
+    order = prompt("what do you have wedding or reception ?");
+
+}
+
+var itemorder = "";
+
+if (order === "wedding") {
+     itemoreder=<img scr='https://lh3.googleusercontent.com/proxy/KxtFgvyES7DSgKVTCG9lQTPNaNXzsiXw9UJAUDcgp-L9QojV_8p6FhyCab7QM5tHkPZCrGYBMQfPs61Hiq83Itd22N61t3tvxtNxkvXy9u3Y02dcpQ'>;</img>
+
+    // addd image
+}else if (order === "reception") {
+    itemorder = <img scr="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.3a2ilati.com%2Farticle%2F%25D8%25B5%25D9%2588%25D8%25B1-%25D9%2588%25D8%25A7%25D9%2581%25D9%2583%25D8%25A7%25D8%25B1-%25D8%25B7%25D8%25A7%25D9%2588%25D9%2584%25D8%25A7%25D8%25AA-%25D8%25A7%25D8%25B3%25D8%25AA%25D9%2582%25D8%25A8%25D8%25A7%25D9%2584-%25D9%2585%25D9%2588%25D8%25A7%25D9%2584%25D9%258A%25D8%25AF-1230819&psig=AOvVaw13_EXxPS1q28AyWJzYifjZ&ust=1612191203966000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPiSkbK2xu4CFQAAAAAdAAAAABAZ"></img>;
+
+}
+var nooforder = prompt("how many the invitees ?");
+
+var result;
+for (var i = 0; i < nooforder; i++); {
+    result = result+itemoreder;
     
-        while (order !== "wedding"&& order =="receptio"){
-             order= prompt ( "what is your celebration wedding or reception?");
-        }
-
-    
+}
 
 
-        <ver more prompt ('do you have any other events yes or no ?' );
-    {while(more!==" no " && more ==" yes ")
-    {more=prompt ("what do you have wedding or reception ?");
-    
-    
 
-       < ver itemorder= " " :
+document.writeresult;
+document.getElementById("order").innerText.order;
+document.getElementById("no of order").innerText.onorder;
 
-       if order=== "wedding"){
-           itemorder = "<img scr="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hiamag.com%2F%25D8%25A7%25D9%2584%25D8%25A3%25D8%25B9%25D8%25B1%25D8%25A7%25D8%25B3%2F%25D8%25AA%25D8%25AC%25D9%2587%25D9%258A%25D8%25B2%25D8%25A7%25D8%25AA-%25D8%25A7%25D9%2584%25D8%25B9%25D8%25B1%25D8%25B3%2F691266-%25D8%25B5%25D9%2588%25D8%25B1-%25D9%2582%25D8%25A7%25D8%25B9%25D8%25A7%25D8%25AA-%25D8%25A7%25D9%2581%25D8%25B1%25D8%25A7%25D8%25AD-%25D9%2581%25D8%25AE%25D9%2585%25D8%25A9-%25D9%2584%25D9%2584%25D8%25A7%25D8%25B9%25D8%25B1%25D8%25A7%25D8%25B3-%25D8%25A7%25D9%2584%25D9%2585%25D9%2584%25D9%2583%25D9%258A%25D8%25A9&psig=AOvVaw3aCcPrEYTGloDl93GPsgNn&ust=1612135843756000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKil9KLoxO4CFQAAAAAdAAAAABAN " >;
-    }else if (order==="reception")
-    {itemorder= "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bayut.com%2Fmybayut%2Far%2F%25D8%25A7%25D9%2584%25D9%2585%25D8%25AD%25D9%2584%25D8%25A7%25D8%25AA-%25D8%25AA%25D9%2588%25D8%25B2%25D9%258A%25D8%25B9%25D8%25A7%25D8%25AA-%25D9%2585%25D9%2588%25D8%25A7%25D9%2584%25D9%258A%25D8%25AF%2F&psig=AOvVaw0oWDLs4S8FOYisKqU9GC0b&ust=1612136168445000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNjJtcrpxO4CFQAAAAAdAAAAABAM "}
-    }
-       < var nooforder = prompt("how many the invitees ?");
+confirm(" are you happy of our services ? ");
+alert(" Thank you for your selections ");
 
-       < var result = result ; itemorder ; > </var>
-       for (var i=0; i<nooforder ; i++);{
-       result= result 
-       }
-       
-         
-
-       document.writeresult;
-       document.getElementById( "order" ).innerText.order ;
-       document.getElementById( "no of order" ).innerText.onorder ;
-
-       confirm (" are you happy of our services ? ");
-       alert(" Thank you for your selections ");
-    
 
 
